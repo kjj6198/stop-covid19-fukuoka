7a32fe074dc1fe820bf642e6457488a124f66484
kjj6198 <kjj6198@gmail.com>
2020-04-30 15:11:59 +0900

    add config

---
+++ src/config.js
@@ -0,0 +1,41 @@
+const detail = {
+  age: {
+    name: '年代',
+    type: 'text',
+    sortable: true,
+    comparator: (a, b) => {
+      const [ageA] = a.match(/(\d+)/);
+      const [ageB] = b.match(/(\d+)/);
+
+      return +ageB - +ageA;
+    },
+  },
+  publishedAt: {
+    name: '公表時間',
+    type: 'text',
+    sortable: true,
+    comparator: (a, b) => new Date(b).getTime() - new Date(a).getTime(),
+  },
+  gender: {
+    name: '性別',
+    type: 'text',
+    formatter: (d) => d[0],
+  },
+  hasDischarged: {
+    name: '退院',
+    sortable: true,
+    comparator: (a, b) => (b === '1' ? 1 : 0),
+    formatter: (text) =>
+      text === '1'
+        ? // TODO: extract
+          `O`
+        : '',
+    type: 'text',
+  },
+  liveAt: {
+    name: '地域',
+    type: 'text',
+  },
+};
+
+module.exports = { detail };

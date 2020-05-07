d90b97614e1ef968dc9568c3083af62603165fd4
kjj6198 <kjj6198@gmail.com>
2020-05-07 11:42:46 +0900

    don't use CDN cache

--- src/server/getData.js
+++ src/server/getData.js
@@ -3,25 +3,24 @@ const request = require('request');
 const getData = (url, options) => {
   return new Promise((resolve, reject) => {
     if (process.env.NODE_ENV === 'production') {
-      request.get(process.env.CDN_URL + url + '.json', function handler(
-        err,
-        response,
-        body
-      ) {
-        if (err) {
-          reject(err);
-        } else {
-          const data = JSON.parse(body);
-          if (url === '/summary') {
-            resolve(data);
+      request.get(
+        process.env.CDN_URL + url + `?now=${Date.now()}` + '.json',
+        function handler(err, response, body) {
+          if (err) {
+            reject(err);
           } else {
-            resolve({
-              totalCount: data.length,
-              data,
-            });
+            const data = JSON.parse(body);
+            if (url === '/summary') {
+              resolve(data);
+            } else {
+              resolve({
+                totalCount: data.length,
+                data,
+              });
+            }
           }
         }
-      });
+      );
     } else {
       request.get('http://localhost:8080/api' + url, options, function handler(
         err,
